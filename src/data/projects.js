df91a716302831e566b434716f5615729f0ce77f
eyob <eyobweldiebelay@gmail.com>
2026-09-22 03:11:17 +0300

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,7 @@
+import register from '../images/re.png'
 import bookImage from '../images/book.jpeg'
 import calculatorImage from '../images/project-3.png'
+
 
 const projects = [
   {
@@ -7,9 +9,9 @@ const projects = [
     description:
       'A web-based student registration and management system designed to make student information and registration processes easier to manage.',
     technologies: ['React', 'Node.js', 'Express', 'MySQL'],
-    image: '/images/project-1.png',
-    liveDemo: '#',
-    github: '#',
+    image: '/images/re.png',
+    liveDemo: 'https://online-m5u4fhp46-eyob-portfolio.vercel.app/',
+    github: 'https://github.com/eyobweldiebelay-cyber/onlineRegister.git',
     type: 'web',
   },
 
@@ -31,7 +33,7 @@ const projects = [
       'An Android application developed using Android Studio as a practical mobile application project.',
     technologies: ['Android Studio', 'Java', 'Kotlin', 'Jetpack Compose'],
     image: bookImage,
-    liveDemo: '#',
+    liveDemo: 'https://drive.google.com/file/d/10yC6pzuwaUu2HIRyy-C1jicsm0bK6XCl/view?usp=drive_link',
     github: '#',
     type: 'android',
   },
